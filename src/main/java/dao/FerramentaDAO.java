e925a0ced59189ac9be86104a40b89f4d1b528fb
CaueFenn <165732840+CaueFenn@users.noreply.github.com>
2024-05-21 20:00:31 -0300

    Documentado FerramentaDAO.java

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -1,16 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
 import java.util.List;
 import model.Ferramenta;
-
+/**
+* (DAO) para a entidade ferramenta.
+* Classe responsável por operações de CRUD como o banco de dados. 
+*
+*/
 public interface FerramentaDAO {
+    /*
+    * salvar uma nova ferramenta no banco de dados.
+    *
+    */
     void salvarFerramenta(Ferramenta ferramenta);
+    /*
+    * atualizar= uma ferramenta existente no banco de dados.
+    *
+    */
     void atualizarFerramenta(Ferramenta ferramenta);
+    /*
+    * exclui uma ferramenta do banco de dados.
+    *
+    */
     void excluirFerramenta(int id);
+    /*
+    * Lista todas as ferramentas no banco de dados.
+    *
+    */
     List <Ferramenta> 
     listarTodasFerramentas();
 }
