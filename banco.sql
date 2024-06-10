2ec75877048b6a1162880accb633ec3046e12621
carloshlohn <165094807+carloshlohn@users.noreply.github.com>
2024-06-10 11:32:08 -0300

    fix banco.sql

--- banco.sql
+++ banco.sql
@@ -1,37 +1,42 @@
-create database db_A3;
-use db_A3;
+CREATE DATABASE db_A3;
+USE db_A3;
 CREATE TABLE amigos (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(100) NOT NULL,
   telefone VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id))
-ENGINE = InnoDB;
+  PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
 CREATE TABLE ferramentas (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(50) NOT NULL,
   marca VARCHAR(50) NOT NULL,
-  preço DECIMAL(7,2) NOT NULL,
-  PRIMARY KEY (id))
-ENGINE = InnoDB;
-CREATE TABLE IF NOT EXISTS emprestimo (
-  id_emprestimo INT auto_increment PRIMARY KEY,
-  id_ferramenta INT NOT NULL,
-  id_amigos INT NOT NULL,
-  data_emprestimo timestamp default NULL,
-  data_limite DATE NOT NULL,
-  amigos_id INT NOT NULL,
-  ferramentas_id INT NOT NULL,
-  CONSTRAINT fk_table1_amigos
-    FOREIGN KEY (amigos_id)
-    REFERENCES amigos (id),
- FOREIGN KEY (ferramentas_id)
-    REFERENCES ferramentas (id))
-    ENGINE = InnoDB;
+  custo DECIMAL(9,2) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE emprestimo (
+  id_emprestimo INT AUTO_INCREMENT PRIMARY KEY,
+  data_emprestimo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  data_devolucao DATE NOT NULL,
+  status BOOLEAN NOT NULL DEFAULT TRUE,
+  nome_amigo VARCHAR(100),
+  nome_ferramenta VARCHAR(50)
+) ENGINE = InnoDB;
+
 CREATE TABLE evento (
-id_evento INT AUTO_INCREMENT PRIMARY KEY,
-id_amigo INT,
-data_evento DATE NOT NULL,
-local_ VARCHAR(100) NOT NULL,
-descricao TEXT,
-foreign key (id_amigo) references amigos(id)
-);
+  id_evento INT AUTO_INCREMENT PRIMARY KEY,
+  id_amigo INT,
+  data_evento DATE NOT NULL,
+  local_evento VARCHAR(100) NOT NULL,
+  descricao TEXT,
+  FOREIGN KEY (id_amigo) REFERENCES amigos(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE ferramenta_emprestimo (
+  id_ferramenta INT,
+  id_emprestimo INT,
+  PRIMARY KEY (id_ferramenta, id_emprestimo),
+  FOREIGN KEY (id_ferramenta) REFERENCES ferramentas(id),
+  FOREIGN KEY (id_emprestimo) REFERENCES emprestimo(id_emprestimo)
+) ENGINE = InnoDB;
